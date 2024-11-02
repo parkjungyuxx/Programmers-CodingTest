a7b46bb8469d3e5c71325f7d5715c8f8dde828dd
Jungyu Park <parkjungyu09@gmail.com>
2024-11-02 21:56:28 +0900

    coding test: level0

---
+++ level1/64.js
@@ -0,0 +1,27 @@
+// 1부터 13까지의 수에서, 1은 1, 10, 11, 12, 13 이렇게 총 6번 등장합니다. 정수 i, j, k가 매개변수로 주어질 때, 
+// i부터 j까지 k가 몇 번 등장하는지 return 하도록 solution 함수를 완성해주세요.
+
+const solution = (i,j,k) => {
+    let count = 0;
+    const numToString = String(k)
+    
+    for (let num = i; num <= j; num++) {
+        const strNum = String(num)
+        for (const char of strNum) {
+            if (char === numToString) {
+                count ++
+            }
+        }
+    } return count;
+}
+
+
+// function solution(i, j, k) {
+//     let a ='';
+//     for(i;i<=j;i++){
+//         a += i;
+//     }
+
+//     return a.split(k).length-1;
+// }
+
