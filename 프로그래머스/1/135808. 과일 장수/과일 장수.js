39e5e6e3f54a8d78f43872332833f2ba5b74a521
Jungyu Park <124859300+parkjungyuxx@users.noreply.github.com>
2025-01-23 00:15:10 +0900

    [level 1] Title: 과일 장수, Time: 337.33 ms, Memory: 93.5 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,11 +1,12 @@
 function solution(k, m, score) {
-    const sortedScore = score.sort((a, b) => b - a);
-
+    const sortedScore = score.sort((a,b) => b-a);
+    const numberOfBox = Math.floor(score.length/m)
+    
     let result = 0;
-    for (let i = 0; i < Math.floor(score.length / m); i++) {
-        const box = sortedScore.slice(i * m, i * m + m)
-        const minScore = box[m - 1]; 
-        result += minScore * m; 
+    for (let i = 0; i < numberOfBox; i++) {
+        const box = sortedScore.slice(i*m, i*m+m);
+        result += box[m-1] * m;
     }
+    
     return result;
 }
