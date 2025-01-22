5b27ea122be451697f853e71537cde9216f8272a
Jungyu Park <124859300+parkjungyuxx@users.noreply.github.com>
2025-01-22 23:20:58 +0900

    [level 1] Title: 소수 찾기, Time: 57.70 ms, Memory: 44.9 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -1,14 +1,14 @@
-function solution(n) {
-    const isPrime = Array(n + 1).fill(true); 
-    isPrime[0] = isPrime[1] = false; 
-
-    for (let i = 2; i * i <= n; i++) {
-        if (isPrime[i]) {
+const solution = (n) => {
+    const isPrime = Array(n+1).fill(true);
+    isPrime[0] = isPrime[1] = false
+    
+    
+    for (let i = 2; i <= n; i++) {
+        if(isPrime[i]) {
             for (let j = i * i; j <= n; j += i) {
-                isPrime[j] = false; 
+                isPrime[j] = false
             }
         }
     }
-
-    return isPrime.filter(Boolean).length; 
+    return isPrime.filter((el) => el===true).length
 }
