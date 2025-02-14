ca164a5fb591e49555f28a5cb31e59f95d8c7cdf
Jungyu Park <124859300+parkjungyuxx@users.noreply.github.com>
2025-02-14 21:48:50 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
@@ -0,0 +1,8 @@
+function solution(my_string, alp) {
+    return my_string.split("").map((el) => {
+        if (el === alp) {
+            return el.toUpperCase()
+        }
+        return el
+    }).join("")
+}
