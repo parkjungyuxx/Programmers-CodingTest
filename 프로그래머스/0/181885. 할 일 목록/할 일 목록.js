85d6f1e2a31552c70d351b9184e1f58f1c3fc2be
Jungyu Park <124859300+parkjungyuxx@users.noreply.github.com>
2025-02-15 16:47:29 +0900

    [level 0] Title: 할 일 목록, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
@@ -0,0 +1,8 @@
+function solution(todo_list, finished) {
+    const result = [];
+    
+    for (let i = 0; i < finished.length; i++) {
+        if (finished[i] === false) result.push(todo_list[i])
+    }
+    return result
+}
