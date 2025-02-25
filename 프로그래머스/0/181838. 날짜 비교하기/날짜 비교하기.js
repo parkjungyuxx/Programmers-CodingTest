705e121544e32ad3d45fd5ace0382b6870df0cce
Jungyu Park <124859300+parkjungyuxx@users.noreply.github.com>
2025-02-25 21:44:44 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -0,0 +1,9 @@
+function solution(date1, date2) {
+    if (date1[0] < date2[0]) return 1; 
+    if (date1[0] === date2[0] && date1[1] < date2[1]) return 1;
+    if (date1[0] === date2[0] && date1[1] === date2[1] && date1[2] < date2[2]) return 1; 
+    
+    return 0; 
+}
+
+
