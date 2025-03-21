712464f955f80b180ab90cfad3c61d26f2de79c1
Jungyu Park <124859300+parkjungyuxx@users.noreply.github.com>
2025-03-21 22:23:24 +0900

    [level 0] Title: 배열 만들기 4, Time: 6.03 ms, Memory: 38.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,10 @@
+function solution(arr) {
+    const stk = [];
+    for (let i = 0; i < arr.length; i++) {
+        while (stk.length > 0 && stk[stk.length-1] >= arr[i]) {
+            stk.pop();
+        }
+        stk.push(arr[i])
+    }
+    return stk;
+}
