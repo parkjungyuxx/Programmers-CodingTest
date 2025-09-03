077a11d48ab1d124de88a203ecefc18a0b8f2713
micky <124859300+parkjungyuxx@users.noreply.github.com>
2025-09-03 17:03:41 +0900

    [level 1] Title: 실패율, Time: 12.05 ms, Memory: 43.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,26 +1,27 @@
 function solution(N, stages) {
-    // 실패율 = 스테이지에 도달 했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어의 수
-    // 1. 도전자 수
-    // 2. 실패율
+    // 1. 플레이어 수
+    // 2. 스테이지 총 수
+    // 3. 실패율
     
-    const challengers = new Array(N+2).fill(0)
+    let challengers = new Array(N + 2).fill(0)
     
     for (const stage of stages) {
         challengers[stage] += 1
+        
     }
     
     const fails = {}
-    let sLength = stages.length
+    let total = stages.length
     
     for (let i = 1; i <= N; i++) {
         if (challengers[i] === 0) {
-            fails[i] = 0;
-            continue;
+            fails[i] = 0
+            continue
         }
-        
-        fails[i] = challengers[i] / sLength
-        sLength -= challengers[i]
+        fails[i] = challengers[i] / total
+        total -= challengers[i]
     }
-    const result = Object.entries(fails)
-    return result.sort((a,b) => b[1] - a[1]).map((v) => Number(v[0]))
+    const result = Object.entries(fails).sort((a,b) => b[1] - a[1])
+    
+    return result.map((v) => Number(v[0]))
 }
