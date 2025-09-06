383c85403321b01d4b86c3fd0ad0ccd8d87c5f88
micky <124859300+parkjungyuxx@users.noreply.github.com>
2025-09-06 22:57:26 +0900

    [level 1] Title: 카드 뭉치, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,17 +1,15 @@
 function solution(cards1, cards2, goal) {
+    let num1 = 0
+    let num2 = 0
     
-    let idx1 = 0, idx2 = 0;
-    
-    for (let word of goal) {
-        if (idx1 < cards1.length && cards1[idx1] === word) {
-            idx1++;
-        }
-        else if (idx2 < cards2.length && cards2[idx2] === word) {
-            idx2++;
-        }
-        else {
-            return "No";
+    for (let i = 0; i < goal.length; i++) {
+        if (goal[i] === cards1[num1]) {
+            num1++
+        } else if (goal[i] === cards2[num2]) {
+            num2++
+        } else {
+            return "No"
         }
     }
-    return "Yes";
+    return "Yes"
 }
