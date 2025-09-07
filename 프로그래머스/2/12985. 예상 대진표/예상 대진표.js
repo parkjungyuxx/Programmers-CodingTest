88f407d1945f9bdf2f5173827d53ca432c63efc4
micky <124859300+parkjungyuxx@users.noreply.github.com>
2025-09-07 23:29:01 +0900

    [level 2] Title: 예상 대진표, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,11 +1,11 @@
 const solution = (n,a,b) => {
-    let round = 0;
+    let answer = 0
     
     while (a !== b) {
-        a = Math.ceil(a/2);
+        a = Math.ceil(a/2)
         b = Math.ceil(b/2)
-        round ++
-        
+        answer++
     }
-    return round
+    return answer
 }
+
