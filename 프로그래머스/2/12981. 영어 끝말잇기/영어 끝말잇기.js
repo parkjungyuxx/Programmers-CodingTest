6f675137f449dcfea78b57b4d563a1deb5abef81
micky <124859300+parkjungyuxx@users.noreply.github.com>
2025-09-10 17:58:41 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,19 +1,17 @@
 function solution(n, words) {
-    const usedWords = new Set();
+    const usedWords = new Set()
+    let prevWord = words[0][0]
     
     for (let i = 0; i < words.length; i++) {
         const word = words[i]
-        const prevWord = words[i-1]
-        
-        if (i > 0 && word[0] !== prevWord[prevWord.length-1]) {
-            return [(i % n)+1, Math.floor(i/n)+1]
+        if (usedWords.has(word) || word[0] !== prevWord) {
+            const player = (i % n) + 1
+            const turn = Math.floor(i / n) + 1
+            return [player, turn]
+        } else {
+            usedWords.add(word)
+            prevWord = word.slice(-1)
         }
-        
-        if (usedWords.has(word)) {
-            return [(i % n)+1, Math.floor(i/n)+1]
-        }
-        
-        usedWords.add(word)
     }
-    return [0, 0]
+    return [0,0]
 }
