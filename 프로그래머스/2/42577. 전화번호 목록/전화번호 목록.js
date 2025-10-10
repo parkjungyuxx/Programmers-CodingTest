c156879c8ffe73395792c581a4dd9ef44dc6068b
micky <124859300+parkjungyuxx@users.noreply.github.com>
2025-10-10 21:22:07 +0900

    [level 2] Title: 전화번호 목록, Time: 522.43 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,8 +1,9 @@
 function solution(phone_book) {
-    phone_book.sort()
+    phone_book.sort((a,b) => a.localeCompare(b))
     
     for (let i = 0; i < phone_book.length - 1; i++) {
-        if (phone_book[i+1].startsWith(phone_book[i])) return false 
+        if (phone_book[i + 1].startsWith(phone_book[i])) return false
     }
+    
     return true
 } 
