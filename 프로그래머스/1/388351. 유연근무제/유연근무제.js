280473379ed3f2188d22f06a1e29027ec99263e8
micky <124859300+parkjungyuxx@users.noreply.github.com>
2025-11-12 23:57:11 +0900

    [level 1] Title: 유연근무제, Time: 1.74 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.js
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.js
@@ -1,29 +1,4 @@
 function solution(schedules, timelogs, startday) {
-  const toMin = (hhmm) => Math.floor(hhmm / 100) * 60 + (hhmm % 100);
-
-  const isWeekday = (dayIdx) => {
-    const day = ((startday - 1 + dayIdx) % 7) + 1;
-    return day >= 1 && day <= 5; 
-  };
-
-  let winners = 0;
-
-  for (let i = 0; i < schedules.length; i++) {
-    const limit = toMin(schedules[i]) + 10; 
-
-    let ok = true;
-    for (let d = 0; d < 7; d++) {
-      if (!isWeekday(d)) continue; 
-
-      const arrive = toMin(timelogs[i][d]);
-      if (arrive > limit) {        
-        ok = false;
-        break;
-      }
-    }
-
-    if (ok) winners++;
-  }
-
-  return winners;
+    var answer = 0;
+    return answer;
 }
